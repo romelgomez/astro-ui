65dea01268ba064c4e204afed93f13ba26512fd8
romelgomez <bmxandcode@gmail.com>
2024-08-11 22:40:28 -0500

    chore: add keys

--- src/components/Accordion/AccordionTwo.tsx
+++ src/components/Accordion/AccordionTwo.tsx
@@ -17,7 +17,7 @@ export function AccordionTwo({ data }: Props) {
     <BaseAccordion type="single" collapsible className="w-full">
       {data.map((item, i) => {
         return (
-          <AccordionItem value={`item-${i.toFixed()}`}>
+          <AccordionItem value={`item-${i.toFixed()}`} key={i}>
             <AccordionTrigger>{item.title}</AccordionTrigger>
             <AccordionContent>{item.description}</AccordionContent>
           </AccordionItem>
