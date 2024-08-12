bf63e3d4119e66fe442f8d7d3e50d6fac6ca84e4
romelgomez <bmxandcode@gmail.com>
2024-08-11 22:48:13 -0500

    chore: add node server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind({
-    applyBaseStyles: false,
-  }),]
+    applyBaseStyles: false
+  })],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  }),
+  server: {
+    host: '0.0.0.0'
+  },
 });
